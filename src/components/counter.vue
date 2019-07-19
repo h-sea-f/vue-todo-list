0fc904584c98d6c8e731c61f721a039427bb5c38
h-sea-f <Hehaifeng2019>
2019-07-19 15:42:54 +0800

    finish pritace

---
+++ src/components/counter.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="counter">
+        <button v-on:click = "decrease">-</button>
+        <span>{{ count }}</span>
+        <button v-on:click = "increase">+</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'counter',
+        data() {
+            return {
+                count: 0,
+            }
+        },
+        methods: {
+            decrease() {
+                this.count--;
+            },
+            increase() {
+                this.count++;
+            }
+        }
+    }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    #counter {
+    text-align: center;
+    }
+</style>
