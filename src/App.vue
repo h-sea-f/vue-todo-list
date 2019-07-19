9936c78ec3877b4417f41dbb6b725033f868974c
h-sea-f <Hehaifeng2019>
2019-07-19 17:07:42 +0800

    not finish pracice 3

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,26 @@
   <div id="app">
 <!--    <img alt="Vue logo" src="./assets/logo.png">-->
 <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <ul id="example-1">
-      <li v-for="item in 2">
-        <counter></counter>
-      </li>
-    </ul>
-
+<!--      <input type="text" v-model="counterNumber">-->
+<!--    <ul id="example-1">-->
+<!--      <li v-for="item in 5">-->
+<!--        <counter></counter>-->
+<!--      </li>-->
+<!--    </ul>-->
+    <counterSum></counterSum>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import counter from './components/counter.vue'
-
+import counterGroup from './components/counterGroup.vue'
+import counterSum from './components/counterSum.vue'
 export default {
   name: 'app',
   components: {
     // HelloWorld,
-    counter
+    counter, counterGroup,counterSum
   },
 
 }
---
+++ src/components/counterSum.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="counterSum">
+        <p></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'counter',
+        data() {
+            return {
+                count: 0,
+            }
+        },
+        methods: {
+            decrease() {
+                this.count--;
+            },
+            increase() {
+                this.count++;
+            }
+        }
+    }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    #counterSum {
+        text-align: center;
+    }
+</style>
