090b4be2a533b88bc4fa59f1a4667c236e773359
h-sea-f <Hehaifeng2019>
2019-07-23 14:14:28 +0800

    使用mapMutations

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -2,30 +2,34 @@
     <div>
         <ul id="filters">
             <li>
-                <a href="#" data-filter="all" @click="showAll">ALL</a>
+                <a href="#" data-filter="all" @click="updateStatus(0)">ALL</a>
             </li>
             <li>
-                <a href="#" data-filter="active" @click="showActive">Active</a>
+                <a href="#" data-filter="active" @click="updateStatus(1)">Active</a>
             </li>
             <li>
-                <a href="#" data-filter="complete" @click="showCompleted">Complete</a>
+                <a href="#" data-filter="complete" @click="updateStatus(2)">Complete</a>
             </li>
         </ul>
     </div>
 </template>
 <script>
+    import { mapMutations } from 'vuex'
     export default {
         name: 'Foot',
         methods: {
-            showAll(){
-                this.$store.commit('updateStatus',0);
-            },
-            showActive(){
-                this.$store.commit('updateStatus',1);
-            },
-            showCompleted(){
-                this.$store.commit('updateStatus',2);
-            }
+            ...mapMutations([
+                'updateStatus'
+            ]),
+            // showAll(){
+            //     this.$store.commit('updateStatus',0);
+            // },
+            // showActive(){
+            //     this.$store.commit('updateStatus',1);
+            // },
+            // showCompleted(){
+            //     this.$store.commit('updateStatus',2);
+            // }
         }
 
     }
