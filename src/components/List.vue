b23a4dd9dae8822a098c373ed9efbf9d0c1d5d6e
h-sea-f <Hehaifeng2019>
2019-07-23 20:42:23 +0800

    解决不能修改item被选中的状态

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,7 +3,7 @@
         <ol>
             <li v-for="(item, index) in getShowList" v-bind:key="item.id" v-bind:class="{checked:item.completed}"
                 :id="item.id">
-                <input name="done-todo" type="checkbox" class="done-todo" :id="item.id" v-model="item.completed" @change="changeCompleted">
+                <input name="done-todo" type="checkbox" class="done-todo" :id="item.id" :checked="item.completed" @change="changeCompleted">
                 <span v-bind:contenteditable="true" @blur="updateItem" :id="item.id" >{{item.content}}</span>
                 <button @click="deleteItem" :id="item.id">删除</button>
             </li>
@@ -32,7 +32,7 @@
                 this.$store.dispatch('deleteItem',e.target.id);
             },
             changeCompleted(e){
-                this.$store.dispatch('updateItem',{id:e.target.id,completed:!e.completed})
+                this.$store.dispatch('updateItem',{id:e.target.id,completed:e.target.checked})
             }
 
         }
